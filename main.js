3bec08a0dcf35bfddb7a6a1926c798b13906b8aa
Vaibhavyadunath <78148244+Vaibhavyadunath@users.noreply.github.com>
2021-04-15 22:19:40 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -36,3 +36,119 @@ function new_image(get_image)
         canvas.add(block_image_object);
     });
 }
+window.addEventListener("keydown",my_keydown);
+
+function my_keydown(e) {
+    keyPressed = e.keyCode;
+    console.log(keyPressed);
+    if (e.shiftKey == true && keyPressed == '80'){
+        console.log("p and shift pressed together");
+        block_image_width = block_image_width + 10;
+        block_image_height = block_image_height + 10;
+        document.getElementById("current_width").innerHTML = block_image_width;
+        document.getElementById("current_height").innerHTML = block_image_height;
+    }
+    if (e.shiftKey == true && keyPressed == '65')
+    {
+        console.log("a and shift pressed together");
+        block_image_width = block_image_width - 10;
+        block_image_height = block_image_height - 10;
+        document.getElementById("current_width").innerHTML = block_image_width;
+        document.getElementById("current_height").innerHTML = block_image_height;
+    }
+
+    if (keyPressed == '38')
+    {
+        up();
+        console.log("up");
+    }
+    if (keyPressed == '40') {
+        down();
+        console.log("down");
+    }
+    if (keyPressed == '37')
+    {
+        left();
+        console.log("left");
+    }
+    if (keyPressed == '39')
+    {
+        right();
+        console.log("right");
+    }
+    if (keyPressed == '70')
+{
+    new_image('ironman_face.png');
+    console.log("f")
+}
+
+    if (keyPressed == '66')
+    {
+        new_image('ironman_body.png');
+        console.log("b");
+    }
+    if (keyPressed == '76')
+    {
+        new_image('ironman_legs.png');
+        console.log("l");
+    }
+    if (keyPressed == '72')
+    {
+        new_image('ironman_left_hand.png');
+        console.log("h");
+    }
+    if (keyPressed == '82')
+    {
+        new_image('ironman_right_hand.png');
+        console.log("r");
+    }
+    if (keyPressed == '79'){
+        new_image('hulk_face.png');
+        console.log("o");
+    }                   
+    if (keyPressed == '66')
+    {
+        new_image('hulk_left_hand.png');
+        console.log("b")
+    }
+    if (keyPressed == '67')
+    {
+        new_image('hulk_right_hand.png');
+        console.log("c");
+    }
+    if (keyPressed == '68')
+    {
+        new_image('hulk_legs.png');
+        console.log("d")
+    }
+    if (keyPressed == '69')
+    {
+        new_image('hulkd_body.png');
+        console.log("e")
+    }
+    if (keyPressed == '85'){
+        new_image('spiderman_face.png');
+        console.log("u");
+    }
+    if (keyPressed == '86')
+    {
+        new_image('spiderman_left_hand.png');
+        console.log("v")
+    }
+    if (keyPressed == '87')
+    {
+        new_image('spiderman_right_hand.png');
+        console.log("w");
+    }
+    if (keyPressed == '88')
+    {
+        new_image('spiderman_legs.png');
+        console.log("x")
+    }
+    if (keyPressed == '89')
+    {
+        new_image('spiderman_body.png');
+        console.log("y")
+    }
+}
+//fblh//
